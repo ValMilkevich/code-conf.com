c3477a3b22f1febb0a1ea82231868324076cbfd5
illus0r <zor667@gmail.com>
2016-08-26 15:25:42 +0300

    close #377 Agenda js bug

--- javascripts/agenda-formatter.js
+++ javascripts/agenda-formatter.js
@@ -1,21 +1,23 @@
 jQuery(function() {
   var agenda = $('.agenda-formatter');
-  var agendaLines = agenda.html().split("\n");
-  var columns = [];
-  agenda.text("");
-  for(i in agendaLines){
-    var line = agendaLines[i];
-    var isTime = /^\d{1,2}:\d{2}/.test(line);
-    if(isTime){
-      printAgendaTable(agenda, columns);
-      columns = [];
-      agenda.append("<div class='time'>"+line+"</div>");
-    } 
-    else{
-      columns.push(line);
+  if (agenda.length){
+    var agendaLines = agenda.first().html().split("\n");
+    var columns = [];
+    agenda.text("");
+    for(i in agendaLines){
+      var line = agendaLines[i];
+      var isTime = /^\d{1,2}:\d{2}/.test(line);
+      if(isTime){
+        printAgendaTable(agenda, columns);
+        columns = [];
+        agenda.append("<div class='time'>"+line+"</div>");
+      } 
+      else{
+        columns.push(line);
+      }
     }
+    printAgendaTable(agenda, columns);
   }
-  printAgendaTable(agenda, columns);
 });
 
 function printAgendaTable(agenda, columns){
